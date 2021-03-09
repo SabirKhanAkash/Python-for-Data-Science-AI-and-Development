c4494bae0dfcf442ba1e6020baa0d197e44b5b9c
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-03-09 21:09:11 +0600

    numpy codes added to main

--- BasicPracticesOfWeek_4.py
+++ BasicPracticesOfWeek_4.py
@@ -93,3 +93,32 @@ import matplotlib.pyplot as plt
 
 plt.plot(x,y)
 plt.show()
+
+A = [[11,12,13], [21,22,23],[31,32,33]]
+
+a = np.array(A)
+print(a)
+print(a.ndim)
+print(a.shape)
+print(a.size)
+a.sort()
+
+print(a[0:2,2])
+
+a=np.array([-1,1])
+b=np.array([1,1])
+print(np.dot(a,b))
+
+X=np.array([[1,0,1],[2,2,2]])
+out=X[0:2,2]
+print(out)
+
+X=np.array([[1,0],[0,1]])
+Y=np.array([[2,2],[2,2]])
+Z=np.dot(X,Y)
+
+print(Z)
+
+X=np.array([[1,0,1],[2,2,2]])
+out=X[0,1:3]
+print(out)
